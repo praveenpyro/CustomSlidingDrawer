5b3485d188115e4d72d3cc1c497836f333e67363
Prasham <prash2488@gmail.com>
2013-01-23 12:31:11 +0530

    Signed-off-by: Prasham <prash2488@gmail.com>

---
+++ gen/com/celites/libraries/customslidingdrawer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.celites.libraries.customslidingdrawer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
